fefb54a0fcc614b9ffea49996979fd5aad6beed2
faishalarif2 <faishalarifn02@gmail.com>
2022-10-18 13:45:02 +0700

    Transaksi JPA

--- src/main/java/com/example/learnmigratedb/TransaksiJpaController.java
+++ src/main/java/com/example/learnmigratedb/TransaksiJpaController.java
@@ -12,6 +12,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Query;
 import javax.persistence.EntityNotFoundException;
+import javax.persistence.Persistence;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
@@ -24,7 +25,7 @@ public class TransaksiJpaController implements Serializable {
     public TransaksiJpaController(EntityManagerFactory emf) {
         this.emf = emf;
     }
-    private EntityManagerFactory emf = null;
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("com.example_learnmigratedb_jar_0.0.1-SNAPSHOTPU");
 
     public EntityManager getEntityManager() {
         return emf.createEntityManager();
